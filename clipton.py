bfd38e6ac37c1f56d472d0f5c0741240837cff6d
Auric Vente <mprops@protonmail.com>
2021-10-12 21:51:10 -0600

    Annotations

--- clipton.py
+++ clipton.py
@@ -4,8 +4,19 @@ import sys
 import json
 from pathlib import Path
 from subprocess import Popen, PIPE
+from typing import List
 
-def get_items():
+# How many items to store in the file
+max_items = 500
+
+# Items are held here internally
+items: List[str]
+
+# Path to the json file
+filepath: Path
+
+# Read the items file and parse it to json
+def get_items() -> None:
   global items
   global filepath
   thispath = Path(__file__).parent.resolve()
@@ -18,12 +29,16 @@ def get_items():
   items = json.loads(content)
   file.close()
 
+# Stringify the json object and save it into the file
 def update_file() -> None:
   file = open(filepath, "w")
   file.write(json.dumps(items))
   file.close()
 
-def add_item(content):
+# Add an item to the items array
+# It performs some checks
+# It removes duplicates
+def add_item(content: str) -> None:
   global items
   content = content.strip()
   if content == "":
@@ -35,8 +50,9 @@ def add_item(content):
   items = items[0:max_items]
   update_file()
 
-def show_picker():
-  opts = []
+# Show the rofi menu with the items
+def show_picker() -> None:
+  opts: List[str] = []
   
   for item in items:
     nlines = item.count("\n") + 1
@@ -55,19 +71,19 @@ def show_picker():
 
   ans = p1.communicate(options)[0].strip()
 
-  if ans == "":
-    return -1
+  if ans != "":
+    on_selection(int(ans))
 
-  index = int(ans)
-  print(index)
-  oitem = items[index]
-  item = oitem.encode("unicode_escape").decode("utf-8")
-  os.popen(f"echo '{item}' | xclip -sel clip")
-  del items[index]
-  add_item(oitem)
+# When an item is selected through the rofi menu
+def on_selection(index: int) -> None:
+    oitem = items[index]
+    item = oitem.encode("unicode_escape").decode("utf-8")
+    os.popen(f"echo '{item}' | xclip -sel clip")
+    del items[index]
+    add_item(oitem)  
 
-if __name__ == "__main__":
-  max_items = 500
+# Main function
+def main() -> None:
   mode = "show"
 
   if len(sys.argv) > 1:
@@ -81,3 +97,6 @@ if __name__ == "__main__":
       add_item(os.popen("xclip -o").read())
   elif mode == "show":
     show_picker()
+
+if __name__ == "__main__":
+  main()
