e4d46aadeb94ee324f849f177f59275578e4d66a
Auric Vente <mprops@protonmail.com>
2021-10-14 01:16:04 -0600

    Asterisk mod fix

--- clipton.py
+++ clipton.py
@@ -57,7 +57,7 @@ def show_picker() -> None:
     line = item["text"]
     line = re.sub("<", "&lt;", line)
     line = re.sub("&", "&amp;", line)
-    line = re.sub("[\s*\n\s*]+", "\n", line)
+    line = re.sub("(\s*\n\s*)+", "\n", line)
     line = line.replace("\n", asterisk)
     line = re.sub(" +", " ", line)
     line = re.sub("</span> +", "</span>", line)
