f419f6acc17145e7f7a2df9cbcb44d625635d937
Auric Vente <mprops@protonmail.com>
2023-12-26 11:30:21 -0600

    Mods

--- clipton.py
+++ clipton.py
@@ -21,7 +21,7 @@ VERSION = "6.5"
 
 # Install 'xclip' and 'rofi'
 # Place 'clipton.py' somewhere in your path
-# Compile 'copyevent.c' and place the binary in your path
+# Compile 'copyevent.c' and place the 'copyevent' in your path
 # Compilation command is commented at the top of the file
 # Place 'clipton.service' inside '/usr/lib/systemd/user/'
 # Start the watcher with 'systemctl --user start clipton'
