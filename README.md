12972e81aa2c114fdabf0d8c7b81ccb27a5ae7d4
Auric Vente <mprops@protonmail.com>
2023-12-27 11:05:54 -0600

    Readme mods

--- README.md
+++ README.md
@@ -1,5 +1,91 @@
-![](clipton.jpg)
+<img src="clipton.jpg" width="420">
 
-## Clipboard Manager
+## About
 
-Read the comments in [clipton.py](clipton.py) for all information.
+Clipton helps you go back to previous clipboard text.
+
+It has 2 functions, the Rofi menu and the clipboard watcher.
+
+The clipboard watcher is used to save copied text automatically.
+
+It works by using 'copyevent' to detect clipboard changes.
+
+The Rofi menu is used to view and select saved items.
+
+You can type something to filter the items.
+
+When an item is selected it is copied to the clipboard.
+
+Then you can paste it anywhere you want.
+
+It can delete indidiual items or all items.
+
+It has some extra features like joining items.
+
+It can also convert text automatically.
+
+## Installation
+
+Install 'xclip' and 'rofi' (Third party programs).
+
+Place 'clipton.py' somewhere in your path like '/usr/bin/'.
+
+Compile 'copyevent.c' and place 'copyevent' in your path.
+
+Compilation command is commented at the top of the c file.
+
+Place 'clipton.service' inside '/usr/lib/systemd/user/'.
+
+Start the watcher with 'systemctl --user start clipton'.
+
+Make it autostart with 'systemctl --user enable clipton'.
+
+Launch the Rofi menu with 'clipton.py'.
+
+Add a keyboard shortcut (somehow) to run 'clipton.py'.
+
+For example bind it to 'Ctrl + Backtick'.
+
+The config path is '~/.config/clipton'.
+
+The items, settings, and converters are placed there.
+
+## Settings
+
+The settings file path is '~/.config/clipton/settings.toml'.
+
+It uses the TOML format and is not required to be edited.
+
+Check the 'Settings' class for the default values.
+
+Override the settings you want to change by adding them:
+
+```toml
+max_items = 250
+
+enable_titles = false
+
+rofi_width = "50%"
+```
+
+## Converters
+
+Converters are functions that automatically change copied text into something else.
+
+They are python files that reside in '~/.config/clipton/converters'.
+
+Check out 'converters/youtube_music.py' for an example.
+
+The function that is called is 'convert(text: str) -> str'.
+
+If nothing is converted it must return an empty string.
+
+Add or remove the converter files you want to enable/disable.
+
+There's a script to copy all converters to the config directory.
+
+If the setting 'save_originals' is enabled, the original text is also saved.
+
+It's saved as 'Original :: <text>' and it's placed below the converted text.
+
+If the converted item is no longer the first item, the original is removed.
--- clipton.py
+++ clipton.py
@@ -1,62 +1,7 @@
 #!/usr/bin/env python
 
-VERSION = "9.6"
-
-# Clipton is a clipboard manager for Linux
-# Repo: https://github.com/madprops/clipton
-
-# ABOUT
-
-# This tool helps you go back to previous clipboard text
-# It has 2 functions, the Rofi menu and the clipboard watcher
-# The clipboard watcher is used to save copied text automatically
-# It works by using 'copyevent' to detect clipboard changes
-# The Rofi menu is used to view and select saved items
-# You can type something to filter the items
-# When an item is selected it is copied to the clipboard
-# Then you can paste it anywhere you want
-# It can delete indidiual items or all items
-# It has some extra features like joining items
-# It can also convert text automatically
-
-# INSTALLATION
-
-# Install 'xclip' and 'rofi' (Third party programs)
-# Place 'clipton.py' somewhere in your path like '/usr/bin/'
-# Compile 'copyevent.c' and place 'copyevent' in your path
-# Compilation command is commented at the top of the c file
-# Place 'clipton.service' inside '/usr/lib/systemd/user/'
-# Start the watcher with 'systemctl --user start clipton'
-# Make it autostart with 'systemctl --user enable clipton'
-# Launch the Rofi menu with 'clipton.py'
-# Add a keyboard shortcut (somehow) to run 'clipton.py'
-# For example bind it to 'Ctrl + Backtick'
-# The config path is '~/.config/clipton'
-# The items, settings, and converters are placed there
-
-# SETTINGS
-
-# The settings file path is '~/.config/clipton/settings.toml'
-# It uses the TOML format and is not required to be edited
-# Check the 'Settings' class for the default values
-# Override the settings you want to change by adding them:
-
-# max_items = 250
-# enable_titles = false
-# rofi_width = "50%"
-
-# CONVERTERS
-
-# Converters are functions that automatically change copied text into something else
-# They are python files that reside in '~/.config/clipton/converters'
-# Check out 'converters/youtube_music.py' for an example
-# The function that is called is 'convert(text: str) -> str'
-# If nothing is converted it must return an empty string
-# Add or remove the converter files you want to enable/disable
-# There's a script to copy all converters to the config directory
-# If the setting 'save_originals' is enabled, the original text is also saved
-# It's saved as 'Original :: <text>' and it's placed below the converted text
-# If the converted item is no longer the first item, the original is removed
+VERSION = "9.9"
+# https://github.com/madprops/clipton
 
 import os
 import re
