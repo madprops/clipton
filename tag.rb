81a03127904dc1a31ea9c1c598785ecadbe3a7ef
Auric Vente <mprops@protonmail.com>
2025-11-13 19:39:34 -0600

    Max text mod

--- tag.rb
+++ tag.rb
@@ -2,7 +2,7 @@
 require "git"
 repo = Git.open(".")
 commit_count = repo.log.count
-name = "v#{commit_count}"
+name = "com_#{commit_count}"
 repo.add_tag(name)
 repo.push("origin", name)
 puts "Created tag: #{name}"
